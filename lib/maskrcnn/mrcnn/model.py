c51b15f9f6c4cd275cddb6184549812e1f397df7
aquatom <sonali.kkmk@gmail.com>
2018-10-03 09:59:09 -0700

    modified model.py maskrcnn

--- lib/maskrcnn/mrcnn/model.py
+++ lib/maskrcnn/mrcnn/model.py
@@ -1219,42 +1219,44 @@ def load_image_gt(dataset, config, image_id, augment=False, augmentation=None,
 
     # Augmentation
     # either with img or albumentations
-    if augmentation['albumentations']:
-        from albumentations import Compose
-        aug = Compose(augmentation['albumentations'])
-        augmented = aug(image=image, mask=np.array(mask, np.uint8))
-        image = augmented['image']
-        mask = augmented['mask']
+    if augmentation is not None:
+        if augmentation['albumentations']:
+            from albumentations import Compose
+            aug = Compose(augmentation['albumentations'])
+            augmented = aug(image=image, mask=np.array(mask, np.uint8))
+            image = augmented['image']
+            mask = augmented['mask']
     
     # This requires the imgaug lib (https://github.com/aleju/imgaug)
-    if augmentation['imgaug']:
-        import imgaug
+    if augmentation is not None:
+        if augmentation['imgaug']:
+            import imgaug
 
-        # Augmentors that are safe to apply to masks
-        # Some, such as Affine, have settings that make them unsafe, so always
-        # test your augmentation on masks
-        MASK_AUGMENTERS = ["Sequential", "SomeOf", "OneOf", "Sometimes",
+            # Augmentors that are safe to apply to masks
+            # Some, such as Affine, have settings that make them unsafe, so always
+            # test your augmentation on masks
+            MASK_AUGMENTERS = ["Sequential", "SomeOf", "OneOf", "Sometimes",
                            "Fliplr", "Flipud", "CropAndPad",
                            "Affine", "PiecewiseAffine"]
 
-        def hook(images, augmenter, parents, default):
-            """Determines which augmenters to apply to masks."""
-            return (augmenter.__class__.__name__ in MASK_AUGMENTERS)
+            def hook(images, augmenter, parents, default):
+                """Determines which augmenters to apply to masks."""
+                return (augmenter.__class__.__name__ in MASK_AUGMENTERS)
 
-        # Store shapes before augmentation to compare
-        image_shape = image.shape
-        mask_shape = mask.shape
-        # Make augmenters deterministic to apply similarly to images and masks
-        det = augmentation.to_deterministic()
-        image = det.augment_image(image)
-        # Change mask to np.uint8 because imgaug doesn't support np.bool
-        mask = det.augment_image(mask.astype(np.uint8),
+            # Store shapes before augmentation to compare
+            image_shape = image.shape
+            mask_shape = mask.shape
+            # Make augmenters deterministic to apply similarly to images and masks
+            det = augmentation.to_deterministic()
+            image = det.augment_image(image)
+            # Change mask to np.uint8 because imgaug doesn't support np.bool
+            mask = det.augment_image(mask.astype(np.uint8),
                                  hooks=imgaug.HooksImages(activator=hook))
-        # Verify that shapes didn't change
-        assert image.shape == image_shape, "Augmentation shouldn't change image size"
-        assert mask.shape == mask_shape, "Augmentation shouldn't change mask size"
-        # Change mask back to bool
-        mask = mask.astype(np.bool)
+            # Verify that shapes didn't change
+            assert image.shape == image_shape, "Augmentation shouldn't change image size"
+            assert mask.shape == mask_shape, "Augmentation shouldn't change mask size"
+            # Change mask back to bool
+            mask = mask.astype(np.bool)
 
     # Note that some boxes might be all zeros if the corresponding mask got cropped out.
     # and here is to filter them out
