e9f650b47f74f6244c40c1605b13c2467c9d550b
aloksaxena93 <aloksaxena93@gmail.com>
2019-02-21 12:31:23 -0800

    updates

---
+++ alok/notebooks/calibration/src/rectification.cpp
@@ -0,0 +1,22 @@
+// #include "opencv2/opencv.hpp"
+// #include "opencv2/gpu/gpu.hpp"
+// #include<iostream> 
+
+// #include "StdAfx.h"
+#include <iostream>
+#include "opencv2/opencv.hpp"
+#include "opencv2/gpu/gpu.hpp"
+#include "opencv2/gpu/gpumat.hpp"
+#include "opencv2/core/core.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include <cuda.h>
+#include <cuda_runtime_api.h>
+using namespace std; 
+
+main() 
+{ 
+    cout << "Learning C++ at GeekforGeeks"; 
+    Mat im = imread("/home/ubuntu/alok/repos/cv_research/alok/notebooks/calibration/data")
+
+    // cv::gpu::remap()
+} 
