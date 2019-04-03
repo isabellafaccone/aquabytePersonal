3e7859446d3940013b033a81272577e0391686df
aquatom <thomas@aquabyte.ai>
2019-04-03 11:43:39 -0700

    2019/04/03 commit

--- thomas/gtsf/utils.py
+++ thomas/gtsf/utils.py
@@ -36,15 +36,18 @@ def load_image_annotations(annotation, new_shape, base_dir = "/root/data/gtsf_2.
             mask = cv2.resize(mask, new_shape)
             masked_image = np.expand_dims(mask, axis=2)*image
         else:
-            keypoints[label] = (int(value[0]["geometry"]["x"]/ratio_width), 
+            keypoints[label] = {}
+            keypoints[label]["coord"] = (int(value[0]["geometry"]["x"]/ratio_width), 
                                 int(value[0]["geometry"]["y"]/ratio_height))
+            keypoints[label]["map"] = np.zeros(shape=new_shape)
     
-    keypoints_map = np.zeros_like(mask)
     delta=1
-    for (kp, v) in keypoints.items():
-        keypoints_map[v[1]-delta:v[1]+delta, v[0]-delta:v[0]+delta] = 1
-
-    return masked_image, mask, keypoints, keypoints_map
+    for (kp, value) in keypoints.items():
+        v = value["coord"]
+        value["map"][v[1]-delta:v[1]+delta, v[0]-delta:v[0]+delta] = 1
+        # value["map"] = np.fliplr(value["map"])
+        
+    return masked_image, mask, keypoints
 
 
 def display_image_with_keypoints(image, keypoints, zoom=True):
@@ -165,32 +168,32 @@ def calculate_errors(moving_keypoints, warped_kp_map, translation_vector):
         print("Manhattan distance: {}".format(np.sum(np.abs(ground_truth_keypoints[r] - predicted_keypoints[c]))))
     
     
-    
 def rgb_to_hsv(r, g, b):
-    """ rgb to hsv duh """
-    maxc = max(r, g, b)
-    minc = min(r, g, b)
-    v = maxc
-    if minc == maxc:
-        return 0.0, 0.0, v
-    s = (maxc-minc) / maxc
-    rc = (maxc-r) / (maxc-minc)
-    gc = (maxc-g) / (maxc-minc)
-    bc = (maxc-b) / (maxc-minc)
-    if r == maxc:
-        h = bc-gc
-    elif g == maxc:
-        h = 2.0+rc-bc
+    r, g, b = r/255.0, g/255.0, b/255.0
+    mx = max(r, g, b)
+    mn = min(r, g, b)
+    df = mx-mn
+    if mx == mn:
+        h = 0
+    elif mx == r:
+        h = (60 * ((g-b)/df) + 360) % 360
+    elif mx == g:
+        h = (60 * ((b-r)/df) + 120) % 360
+    elif mx == b:
+        h = (60 * ((r-g)/df) + 240) % 360
+    if mx == 0:
+        s = 0
     else:
-        h = 4.0+gc-rc
-    h = (h/6.0) % 1.0
-    return h, s, v   
+        s = (df/mx)*100
+    v = mx*100
+    return h, s, v
+
     
     
-def create_mask(f, min_hsv, max_hsv):
+def create_mask(f, min_hsv, max_hsv, new_shape=(512, 512)):
     im = Image.open(f)
     im = im.convert('RGBA')
-    im = im.resize((512, 512))
+    im = im.resize(new_shape)
     original = copy.copy(im)
     # im = im.crop([50, 50, 100 ,100])
     pix = im.load()
@@ -198,26 +201,26 @@ def create_mask(f, min_hsv, max_hsv):
     for x in range(width):
         for y in range(height):
             r, g, b, a = pix[x, y]
-            h_ratio, s_ratio, v_ratio = rgb_to_hsv(r / 255.0, g / 255.0, b / 255.0)
-            h, s, v = (h_ratio * 360, s_ratio * 255, v_ratio * 255)
-
+            h, s, v = rgb_to_hsv(r, g, b)
             min_h, min_s, min_v = min_hsv
             max_h, max_s, max_v = max_hsv
             if min_h <= h <= max_h and min_s <= s <= max_s and min_v <= v <= max_v:
-                pix[x, y] = (0, 0, 0, 0)
+                pix[x, y] = (0, 0, 0)
             else:
                 continue
-                # print(h,s,v)
-    mask = np.asarray(im)[..., 3]
+    mask = np.array(im)[..., 0]
     mask.flags.writeable = True
     mask[mask > 0] = 1
     
     # let's erode
-    mask = cv2.erode(mask, np.ones((3, 3)))
+    # mask = cv2.erode(mask, np.ones((3, 3)))
+    mask = cv2.dilate(mask, np.ones((5, 5)))
     
     # let's dilate
-    mask = cv2.dilate(mask, np.ones((3, 3)))
+    # mask = cv2.dilate(mask, np.ones((3, 3)))
     #mask = cv2.dilate(mask, np.ones((3, 3)))
+    mask = cv2.erode(mask, np.ones((3, 3)))
+    mask = cv2.erode(mask, np.ones((3, 3)))
     
     # let's get the fish
     labs = label(mask)
