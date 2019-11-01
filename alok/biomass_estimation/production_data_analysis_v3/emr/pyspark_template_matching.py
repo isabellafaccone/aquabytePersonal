b309cecf42204e12b02a6cb1b90e33041d16029a
Vein Kong <vein@aquabyte.ai>
2019-11-01 11:06:19 -0700

    Fix breaking pieces for pyspark template match

--- alok/biomass_estimation/production_data_analysis_v3/emr/pyspark_template_matching.py
+++ alok/biomass_estimation/production_data_analysis_v3/emr/pyspark_template_matching.py
@@ -20,7 +20,7 @@ REGION="eu-west-1"
 OUTPUT_BUCKET="aquabyte-research"
 
 
-from aquabyte.data_access_utils import S3AccessUtils, RDSAccessUtils
+#from aquabyte.data_access_utils import S3AccessUtils, RDSAccessUtils
 
 
 def enhance(image, clip_limit=5):
@@ -156,7 +156,6 @@ DbParams = namedtuple("DbParams", "user password host port db_name")
 
 def generate_matches_and_homography(left_crop_url, right_crop_url, keypoints, cm, left_crop_metadata, right_crop_metadata):
 
-    left_image_url, right_image_url = df.left_image_url.iloc[0], df.right_image_url.iloc[0]
     imageL = load_image(left_crop_url)
     imageR = load_image(right_crop_url)
     left_crop_metadata = json.loads(left_crop_metadata)
