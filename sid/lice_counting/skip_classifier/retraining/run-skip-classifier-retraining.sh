c07a9011fb467be87c1ae66c528b317d28889ea2
siddsach <siddsach@gmail.com>
2021-03-17 13:10:25 -0700

    updates to cron

--- sid/lice_counting/skip_classifier/retraining/run-skip-classifier-retraining.sh
+++ sid/lice_counting/skip_classifier/retraining/run-skip-classifier-retraining.sh
@@ -4,5 +4,5 @@ TODAY=$(date "+%Y-%m-%d")
 
 echo "Writing logs to /root/data/skip-classifier/logs/$TODAY"
 
-time -p python3 skip-classifier-retraining.py >> /root/data/skip-classifier/logs/$TODAY 2>&1
+time -p /home/user/miniconda/envs/py36/bin/python3 /root/skip_classifier/retraining/skip-classifier-retraining.py >> /root/data/skip-classifier/logs/$TODAY 2>&1
 
